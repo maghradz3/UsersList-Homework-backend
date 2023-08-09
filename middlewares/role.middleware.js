99a180213a6fddef6094660fee794184b958d4b3
Levan Maghradze <120563893+maghradz3@users.noreply.github.com>
2023-08-09 23:42:03 +0400

    back to old middlewear

--- middlewares/role.middleware.js
+++ middlewares/role.middleware.js
@@ -1,6 +1,6 @@
 export const roleMiddleware = async (req, res, next) => {
   let role = req.user.role;
-  if (!role.includes("user")) {
+  if (!role.includes("admin")) {
     res.status(403).json({ message: "You are not authorized for this route!" });
     return;
   }
