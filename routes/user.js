1b3e30f8e2ac216d206420ac364a8144176d8fd6
Levan Maghradze <120563893+maghradz3@users.noreply.github.com>
2023-08-09 22:35:46 +0400

    delete user logic

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,8 @@ import {
   refreshToken,
   deleteUser,
 } from "../controllers/user.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { roleMiddleware } from "../middlewares/role.middleware.js";
 
 const router = express.Router();
 
