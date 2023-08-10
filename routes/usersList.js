fbe8c261d4f90756d003e4a9d7c292aadad47468
Levan Maghradze <120563893+maghradz3@users.noreply.github.com>
2023-08-10 10:07:38 +0400

    change delete logic

--- routes/usersList.js
+++ routes/usersList.js
@@ -1,9 +1,20 @@
 import express from "express";
 import { getMainUsers } from "../controllers/userList.js";
-import { deleteUser } from "../controllers/userList.js";
+// import { deleteUser } from "../controllers/userList.js";
 
 const router = express();
 
-router.get("gsg", getMainUsers);
-router.delete("/:id", deleteUser);
+router.get("/users", getMainUsers);
+router.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    await user.remove();
+
+    res.json({ message: "User deleted" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 export default router;
