d79f0ae68ce448362040c6cb924801de5bef8999
Levan Maghradze <120563893+maghradz3@users.noreply.github.com>
2023-08-10 11:05:26 +0400

    smth

--- controllers/userList.js
+++ controllers/userList.js
@@ -16,3 +16,21 @@ export const getMainUsers = async (req, res) => {
       .json({ message: "Internal server error", users: [] });
   }
 };
+
+export const deleteUser = async (req, res) => {
+  const { id: _id } = req.params;
+  try {
+    const UserExists = await User.exists({ _id });
+    if (UserExists) {
+      await User.findByIdAndRemove(_id);
+
+      return res.status(200).json({ message: "User deleted successfully" });
+    } else {
+      return res
+        .status(404)
+        .json({ message: `User with id of ${_id} does not exists` });
+    }
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+};
