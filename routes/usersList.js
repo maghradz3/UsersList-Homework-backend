05e122231019a306ce9ab994fc4c0400856af8b4
Levan Maghradze <120563893+maghradz3@users.noreply.github.com>
2023-08-10 11:04:00 +0400

    back to old logic

--- routes/usersList.js
+++ routes/usersList.js
@@ -1,20 +1,11 @@
 import express from "express";
 import { getMainUsers } from "../controllers/userList.js";
-// import { deleteUser } from "../controllers/userList.js";
+import { deleteUser } from "../controllers/userList.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = express();
 
 router.get("/users", getMainUsers);
-router.delete("/users/:id", async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (!user) return res.status(404).json({ message: "User not found" });
+router.delete("/users/:id", deleteUser, authMiddleware);
 
-    await user.remove();
-
-    res.json({ message: "User deleted" });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
 export default router;
